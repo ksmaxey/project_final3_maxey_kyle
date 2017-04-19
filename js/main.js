5adb1ffe9d55d1c30c19e28b5e8699c4c2326a13
Kyle Maxey <ksmaxey@gmail.com>
2017-04-18 21:04:10 -0400

    remove comments on main javascript file

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ $(document).ready(function () {
 
 var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('map');
 var modalImg = document.getElementById('img01');
 img.onclick = function () {
@@ -29,10 +28,9 @@ img.onclick = function () {
     modalImg.src = this.src;
 };
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName('close')[0];
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function () {
     modal.style.display = 'none';
 };
